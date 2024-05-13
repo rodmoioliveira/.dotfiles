b784722f6ec1bc1c6d72dd98d8a0bb0cd9934ef0
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2024-05-13 17:05:38 -0300

    feat: add flat-ops commands
    Signed-off-by: Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>

---
+++ .local/bin/_bfs.js
@@ -0,0 +1,37 @@
+const fs = require("node:fs");
+
+const bfs = (node) => {
+  if (node == null || typeof node !== "object") return;
+
+  const queue = [];
+  queue.push({ name: "", n: node });
+
+  while (queue.length > 0) {
+    const o = queue.shift();
+    const { name, n } = o;
+
+    if (n == null) continue;
+    if (typeof n !== "object") {
+      console.log(`${name}\t${typeof n}\t${JSON.stringify(n)}`);
+    }
+
+    if (typeof n === "object") {
+      const keys = Object.keys(n);
+      for (const k of keys) {
+        queue.push({ name: `${name}.${k}`, n: n[k] });
+      }
+    }
+  }
+};
+
+const main = () => {
+  fs.readFile(process.argv[2], "utf8", (error, data) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    bfs(JSON.parse(data));
+  });
+};
+
+main();
---
+++ .local/bin/_dfs.js
@@ -0,0 +1,37 @@
+const fs = require("node:fs");
+
+const dfs = (node) => {
+  if (node == null || typeof node !== "object") return;
+
+  const stack = [];
+  stack.push({ name: "", n: node });
+
+  while (stack.length > 0) {
+    const o = stack.pop();
+    const { name, n } = o;
+
+    if (n == null) continue;
+    if (typeof n !== "object") {
+      console.log(`${name}\t${typeof n}\t${JSON.stringify(n)}`);
+    }
+
+    if (typeof n === "object") {
+      const keys = Object.keys(n);
+      for (const k of keys) {
+        stack.push({ name: `${name}.${k}`, n: n[k] });
+      }
+    }
+  }
+};
+
+const main = () => {
+  fs.readFile(process.argv[2], "utf8", (error, data) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    dfs(JSON.parse(data));
+  });
+};
+
+main();
